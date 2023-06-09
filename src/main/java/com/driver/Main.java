f88642fde89ff49773a5981325e0c0b2ee42a93d
Kunal Jindal <kunaljindal@mnnit.ac.in>
2023-06-09 23:02:37 +0530

    Added Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,7 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         RWOnly obj = new RWOnly();
+
         //obj.name = "Kunal";
         obj.setName("Kunal");
         obj.getName();
